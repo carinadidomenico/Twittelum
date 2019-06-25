0f54036e246d7c05d6cd94b1ccce22e77b1fa7a9
carinadidomenico <carinadidomenico@gmail.com>
2019-06-25 14:38:03 -0300

    Abas

---
+++ src/pages/LoginPage/index.js
@@ -0,0 +1,63 @@
+import React, { Component, Fragment } from 'react'
+import Cabecalho from '../../components/Cabecalho'
+import Widget from '../../components/Widget'
+
+import './loginPage.css'
+
+class LoginPage extends Component {
+    
+    fazerLogin = (e) => {
+        e.preventDefault()
+        console.log (this.inputLogin.value)
+        // console.log (this.inputSenha.value)
+    }
+    
+
+    render() {
+
+        return (
+            <Fragment>
+                <Cabecalho />
+                <div className="loginPage">
+                    <div className="container">
+                        <Widget>
+                            <h2 className="loginPage__title">Seja bem vindo!</h2>
+                            <form className="loginPage__form" action="/" onSubmit={this.fazerLogin}>
+                                <div className="loginPage__inputWrap">
+                                    <label className="loginPage__label" htmlFor="login">Login</label> 
+                                    <input 
+                                        ref={(elementoInput) => this.inputLogin = elementoInput} 
+                                        className="loginPage__input" 
+                                        type="text" 
+                                        id="login" 
+                                        name="login"
+                                    />
+                                </div>
+                                <div className="loginPage__inputWrap">
+                                    <label className="loginPage__label" htmlFor="senha">Senha</label> 
+                                    <input 
+                                        ref={(elementoInput) => this.inputSenha = elementoInput} 
+                                        className="loginPage__input" 
+                                        type="password" id="senha" 
+                                        name="senha"
+                                    />
+                                </div>
+                                {/* <div className="loginPage__errorBox">
+                                    Mensagem de erro!
+                                </div> */}
+                                <div className="loginPage__inputWrap">
+                                    <button className="loginPage__btnLogin" type="submit">
+                                        Logar
+                                    </button>
+                                </div>
+                            </form>
+                        </Widget>
+                    </div>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+
+export default LoginPage
