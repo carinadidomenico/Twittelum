6090b8e02379b0f4652601992cdf2922c5098a20
carinadidomenico <carinadidomenico@gmail.com>
2019-06-26 15:32:59 -0300

    Arrumando rota login/home

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,40 @@
 import React from 'react'
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 
 //Páginas
 import Home from './pages/Home/Home'
 import LoginPage from './pages/LoginPage'
 
 
+function Autenticar () {
+    if (localStorage.getItem('TOKEN')) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+class PrivateRoute extends React.Component {
+    render() {
+        
+        const Component = this.props.component
+        if (Autenticar()) {
+             //componente rota tweets
+             return <Route render={(props) => <Component {...props}></Component>}></Route>
+        }
+        else {
+            //componente rota login
+            return <Redirect to="/login"></Redirect> 
+        }
+    }
+}
+
 const Roteamento = () => {
     return (
         <Switch>
-            <Route exact path='/' component={Home}></Route>
-            <Route exact path='/login' component={LoginPage}></Route>
+            <PrivateRoute exact path='/' component={Home}></PrivateRoute>
+            <Route path='/login' component={LoginPage}></Route>
         </Switch>
     )
 }
