e281b400066e6cbfcf0e88e06cda8f3f9424ff42
carinadidomenico <carinadidomenico@gmail.com>
2019-06-27 16:59:26 -0300

    Configurar dados no tweet postado

--- src/components/Tweet/index.js
+++ src/components/Tweet/index.js
@@ -5,16 +5,19 @@ import './tweet.css'
 class Tweet extends Component {
 
     render() {
+
+        const {usuario, conteudo, totalLikes} = this.props
+
         return (
             <article className="tweet">
                 <div className="tweet__cabecalho">
-                    <img className="tweet__fotoUsuario" src={this.props.usuario.foto} alt="" />
-                    <span className="tweet__nomeUsuario">{this.props.usuario.nome}</span>
-                    <a href="/"><span className="tweet__userName">@{this.props.usuario.login}</span></a>
+                    <img className="tweet__fotoUsuario" src={usuario.foto} alt="" />
+                    <span className="tweet__nomeUsuario">{usuario.nome}</span>
+                    <a href="/"><span className="tweet__userName">@{usuario.login}</span></a>
                 </div>
                 <p className="tweet__conteudo">
                     <span>
-                        {this.props.conteudo}
+                        {conteudo}
                     </span>
                 </p>
                 <footer className="tweet__footer">
@@ -32,7 +35,7 @@ class Tweet extends Component {
                                 <path d="M36.885 25.166c0 5.45-4.418 9.868-9.867 9.868-3.308 0-6.227-1.632-8.018-4.128-1.79 2.496-4.71 4.129-8.017 4.129-5.45 0-9.868-4.418-9.868-9.868 0-.773.098-1.52.266-2.242C2.75 14.413 12.216 5.431 19 2.965c6.783 2.466 16.249 11.448 17.617 19.96.17.721.268 1.47.268 2.241"></path>
                             </g>
                         </svg>
-                        {this.props.totalLikes}
+                        {totalLikes}
                     </button>
                 </footer>
             </article>
