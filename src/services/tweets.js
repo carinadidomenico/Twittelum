604d3bc5289e8c9907f87891c6ae1ae1cf465725
carinadidomenico <carinadidomenico@gmail.com>
2019-06-27 15:24:58 -0300

    Adicionando axios api

---
+++ src/services/tweets.js
@@ -0,0 +1,8 @@
+import protocolo from './api'
+
+export function postTweets(tweet, token) {
+    const url = `/tweets?X-AUTH-TOKEN=${token}`
+    return (
+        protocolo.post(url, tweet)
+    )
+}
